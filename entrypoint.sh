7d9adbf65fcc886e97441a06c65e1834528b52f0
itsvinayak <itssvinayak@gmail.com>
2020-03-25 11:27:51 +0530

    icon and des

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 echo "Running style checks on ${inputs.filename}"
-flake8 ${inputs.filename} >> error.log
+flake8 ${inputs.filename} --count --select=E9,F63,F7,F82 --show-source --statistics
 
 echo "formating ${inputs.filename}"
 black ${inputs.filename}
